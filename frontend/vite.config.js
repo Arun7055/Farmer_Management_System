f23feb11aca0acb452a09232d01ec52880990a22
Sai Arun Kumar Chandrasekar <arun@Sais-MacBook-Air.local>
2026-02-04 18:12:38 +0530

    fixess

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://farmer-management-system-4df3.onrender.com",
         changeOrigin: true,
         secure: false
       }
